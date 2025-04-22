ac1a8c572c3569c03049f3f0162d92f14ee67d0e
mrgeorge2022 <mr.georgesince2022@gmail.com>
2025-04-22 18:40:10 -0500

    Actualizar horario script.js

--- script.js
+++ script.js
@@ -769,7 +769,7 @@ function validateQuantityInput() {
 const horariosTienda = [
   { dia: 0, horaApertura: 18, horaCierre: 24 },  // Domingo
   { dia: 1, horaApertura: 18, horaCierre:24 },  // Lunes 
-  { dia: 2, horaApertura: 22, horaCierre: 24 },  // Martes
+  { dia: 2, horaApertura: 18, horaCierre: 24 },  // Martes
   { dia: 3, horaApertura: null, horaCierre: null},  // Miércoles - cerrdado
   { dia: 4, horaApertura: 18, horaCierre: 24 },  // Jueves 
   { dia: 5, horaApertura: 18, horaCierre: 24 },  // Viernes
